90da2c7d729a2ee42a185daa048c8b969c08c669
bjorndonald <bjorndonaldb@yahoo.com>
2025-04-14 18:37:45 +0100

    perfect finish design

--- src/app/(templates)/confirmation/page.tsx
+++ src/app/(templates)/confirmation/page.tsx
@@ -4,7 +4,7 @@ import tick from '@/assets/images/tick.png'
 import Image from 'next/image'
 const ConfirmationPage = () => {
     return (
-        <div className='bg-gradient-to-b min-h-screen overflow-hidden from-[#C0CBF9] to-[#3232B8] p-6'>
+        <div className='bg-gradient-to-b h-screen overflow-hidden from-[#C0CBF9] to-[#3232B8] p-6'>
             <div className='flex mb-12 items-center justify-between gap-2'>
                 <div className="flex flex-col">
                     <div className=" flex items-center gap-2 text-sm text-gray-600">
@@ -24,14 +24,14 @@ const ConfirmationPage = () => {
             <div className="mx-auto max-w-6xl justify-between flex items-center">
 
 
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between items-center w-[1220px] h-[785px] ">
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-between items-center w-[920px] h-[565px] ">
                     <Image src={paper} alt="paper" className='object-cover  absolute w-full h-full top-0 left-0' />
 
-                    <Image src={tick} alt="tick" className='absolute top-1/2 left-6  -translate-y-1/2' />
+                    <Image src={tick} alt="tick" className='absolute top-1/2 left-0 translate-x-[260%] w-14 h-14 -translate-y-1/2' />
 
-                    <div className="flex flex-col gap-4">
-                        <h2 className='text-3xl font-bold mb-4'>PAYMENT CONFIRMED</h2>
-                        <div className="flex justify-between items-center">
+                    <div className="relative z-40 items-center mx-auto w-full max-w-[480px] flex flex-col  translate-x-[14%]">
+                        <h2 className='text-2xl  font-extrabold mb-4'>PAYMENT CONFIRMED</h2>
+                        <div className="flex w-full justify-between items-center">
                             <span>
                                 Reference number
                             </span>
@@ -39,32 +39,32 @@ const ConfirmationPage = () => {
                                 001922773JK47KF47
                             </span>
                         </div>
-                        <div className="flex justify-between items-center">
+                        <div className="flex w-full justify-between items-center">
                             <span>
-                                Reference number
+                                Date
                             </span>
                             <span>
-                                001922773JK47KF47
-                            </span>
-                        </div>
-                        <div className="flex justify-between items-center">
-                            <span>
-                                Reference number
-                            </span>
-                            <span>
-                                001922773JK47KF47
+                                19 Jan, 2025
                             </span>
                         </div>
-                        <div className="flex justify-between items-center mb-12">
+                        <div className="flex w-full justify-between items-center">
                             <span>
-                                Reference number
+                                Time
                             </span>
                             <span>
-                                001922773JK47KF47
+                                17:53 PM
+                            </span>
+                        </div>
+                        <div className="flex w-full justify-between items-center mb-6">
+                            <span>
+                                Payment method
+                            </span>
+                            <span>
+                                Debit Card
                             </span>
                         </div>
 
-                        <div className="flex justify-between text-4xl font-bold items-center">
+                        <div className="flex w-full justify-between text-2xl font-extrabold items-center">
                             <span>
                                 AMOUNT
                             </span>
